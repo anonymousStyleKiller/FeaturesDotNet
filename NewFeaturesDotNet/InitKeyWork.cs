00283f555b8a6832df0ad8814fd10ec6711bd370
Anton <gumb1t97@gmail.com>
2022-09-10 19:55:11 +0300

    Init for create object

---
+++ NewFeaturesDotNet/InitKeyWork.cs
@@ -0,0 +1,23 @@
+﻿namespace NewFeaturesDotNet;
+
+public class InitKeyWork
+{
+    public void M()
+    {
+        var p = new Person()
+        {
+            Name = "Anton",
+            Surname = "Kharhcenko"
+        };
+        // Hear will be error
+        // p.Name = "new";
+        p.Surname = "Good";
+    }
+   
+}
+
+public class Person
+{
+    public string Name { get; init; }
+    public string Surname { get; set; }
+}
