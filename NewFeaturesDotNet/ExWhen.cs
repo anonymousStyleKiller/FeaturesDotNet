3e770bbfc50c53929ba0c378ad73048c675c5d72
Anton <gumb1t97@gmail.com>
2022-09-11 10:52:42 +0300

    Operator when for exceptions

---
+++ NewFeaturesDotNet/ExWhen.cs
@@ -0,0 +1,17 @@
+﻿namespace NewFeaturesDotNet;
+
+public class ExWhen
+{
+    public void Test()
+    {
+        try
+        {
+
+        }
+        catch (Exception e) when(e.HResult >1000_00)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
